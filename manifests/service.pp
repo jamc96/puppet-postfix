f8c4412c471683f239a72257cd61813282737413
Jose Mejia <jamc.mejia@gmail.com>
2018-04-12 12:02:28 -0600

    hardcoding attributes on postfix service

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,15 +3,12 @@
 class postfix::service(
   $service_name       = $::postfix::service_name,
   $service_ensure     = $::postfix::service_ensure,
-  $service_enable     = $::postfix::service_enable,
-  $service_hasrestart = $::postfix::service_hasrestart,
-  $service_hasstatus  = $::postfix::hasstatus,
   ) {
   # resources
   service { $service_name:
     ensure     => $service_ensure,
-    enable     => $service_enable,
-    hasrestart => $service_hasrestart,
-    hasstatus  => $service_hasstatus,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
   }
 }
